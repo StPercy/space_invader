b84883290a4486561a412623406810d6e7af1e24
Stefan Pertschi <stefan.pertschi@gfn.de>
2024-04-12 10:42:10 +0200

    tutorial minute 12 checked

--- src/index.js
+++ src/index.js
@@ -1,43 +1,72 @@
-import GameScene from './Scenes/GameScene.js'
-import MenuScene from './Scenes/MenuScene.js'
-import HighscoreScene from './Scenes/HighscoreScene.js'
-import PauseScene from './Scenes/PauseScene.js'
-import PreloadScene from './Scenes/PreloadScene.js'
-
-const WIDTH = 1280
-const HEIGHT = 720
-
-const settings = {
-	width: WIDTH,
-	height: HEIGHT,
-	center: {
-		x: WIDTH / 2,
-		y: HEIGHT / 2,
-	},
-	textSpace: 50,
-}
-
 const config = {
 	type: Phaser.AUTO,
-	scale: {
-		mode: Phaser.Scale.FIT,
-		autoCenter: Phaser.Scale.CENTER_BOTH,
-		width: settings.width,
-		height: settings.height,
-	},
+	width: 1280,
+	height: 720,
 	physics: {
 		default: 'arcade',
 		arcade: {
-			debug: true, // false um Umrandungen auszublenden
+			debug: true
 		},
 	},
-	scene: [
-		new PreloadScene(settings),
-		new MenuScene(settings),
-		new HighscoreScene(settings),
-		new GameScene(settings),
-		new PauseScene(settings),
-	],
+	scene: { preload: preload, create: create, update: update},
 }
 
-new Phaser.Game(config)
+const game = new Phaser.Game(config);
+
+let physics;
+let player;
+let aKey;
+let dKey;
+let spacebar;
+
+function preload() {
+	this.load.image('background', 'assets/space_invader/images/space.jpeg');
+	this.load.image('player', 'assets/space_invader/images/Ship.png');
+	this.load.image('bullet', 'assets/space_invader/images/Bullet.png');
+	this.load.image('enemyTop1', 'assets/space_invader/images/InvaderA1.png');
+	this.load.image('enemyTop2', 'assets/space_invader/images/InvaderA2.png');
+	this.load.image('enemyMiddle1', 'assets/space_invader/images/InvaderB1.png');
+	this.load.image('enemyMiddle2', 'assets/space_invader/images/InvaderB2.png');
+	this.load.image('enemyBottom1', 'assets/space_invader/images/InvaderC1.png');
+	this.load.image('enemyBottom2', 'assets/space_invader/images/InvaderC2.png');
+	this.load.image('block', 'assets/space_invader/images/OkBlock.png');
+	this.load.image('blockHit', 'assets/space_invader/images/WeakBlock.png');	
+}
+
+function create() {
+	setPhysics(this.physics);
+	this.add.image(0, 0, 'background').setOrigin(0).setScrollFactor(0);
+	createPlayer();
+	createKeys(this.input.keyboard);
+}
+
+function update() {
+	checkMovement(); 
+}
+// physics wird ausgelagert um sich this.physics zu sparen, siehe z.B createPlayer()
+function setPhysics(physic) {
+	physics = physic;
+}
+
+function createPlayer() {
+	player = physics.add.sprite(640, 650, 'player');
+	player.setDepth(10);
+}
+
+function createKeys(keyboard) {
+	aKey = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A, true, true);
+	dKey = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D, true, true);
+	spacebar = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE, true, true);
+}
+
+function checkMovement() {
+	if (aKey.isDown && !dKey.isDown) {
+		player.x > 30 ? player.setVelocityX(-300) : player.setVelocityX(0);
+	}
+	if (dKey.isDown && !aKey.isDown) {
+		player.x < 1250 ? player.setVelocityX(300) : player.setVelocityX(0);
+	}
+	if (!aKey.isDown && !dKey.isDown) {
+		player.setVelocityX(0);
+	}
+}
